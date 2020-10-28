c44ed602e2896a76228e5a28252096d6b75f8444
QVDev <feedbackandroid@gmail.com>
2020-10-27 22:46:45 -0600

    performance testing improvements

--- pip.js
+++ pip.js
@@ -1,10 +1,10 @@
 
 let glOptions = {
     alpha: true,
-    antialias: true,
-    depth: true,
+    antialias: false,
+    depth: false,
     failIfMajorPerformanceCaveat: false,
-    powerPreference: "default",
+    powerPreference: "high-performance",
     premultipliedAlpha: true,
     preserveDrawingBuffer: false,
     stencil: false,
@@ -13,18 +13,17 @@ let glOptions = {
 
 let options = {
     endOnLastSourceEnd: false,
-    useVideoElementCache: false,
-    videoElementCacheSize: 0,
+    useVideoElementCache: true,
+    videoElementCacheSize: 6,
     webglContextAttributes: glOptions
 }
 
 const url = 'https://gist.githubusercontent.com/jsturgis/3b19447b304616f18657/raw/a8c1f60074542d28fa8da4fe58c3788610803a65/gistfile1.txt'
 fetch(url).then(function (response) {
     response.text().then(function (text) {
-        text = text.replace('var mediaJSON = ','');
+        text = text.replace('var mediaJSON = ', '');
         text = text.slice(0, -1)
         var videos = JSON.parse(text);
-        console.log(videos);
         addVideos(videos.categories[0].videos);
     });
 });
@@ -58,8 +57,6 @@ function addVideos(videos) {
 
 
 async function start() {
-    console.log("PIP");
-
     var x = -0.8;
     var y = -0.8;
 
@@ -73,9 +70,6 @@ async function start() {
         translateNode.positionOffsetY = y;
 
         x = x + 0.4;
-
-        console.log("x::" + x);
-        console.log("y::" + y);
 
         if (x > 1.2) {
             x = -0.8;
@@ -94,10 +88,10 @@ async function start() {
     await play();
 }
 
-async function play() {
-    // audioTimerLoop(anim, 1000 / 60);
-    // videoCtx._useVideoElementCache = false
+const CANVAS = true;
+var refVideo;
 
+async function play() {
     await videoCtx.play();
     setTimeout(function () {
         // if (videoCtx.state == VideoContext.STATE.PLAYING) {
@@ -108,7 +102,48 @@ async function play() {
         video.srcObject = stream;
         video.hidden = false;
         video.onloadedmetadata = (event) => {
-            video.requestPictureInPicture();
+
+            if (CANVAS) {
+                updateCanvas();
+            } else {
+                video.requestPictureInPicture();
+            }
         };
+        if (!CANVAS) {
+            refVideo = document.getElementsByTagName("video")[1]
+            refVideo.requestVideoFrameCallback(updateVideo);
+        }
     }, 500);
 }
+
+let paintCount = 0;
+var startTime = 0.0;
+let fpsInfo = document.getElementById("fps-info");
+function updateVideo(now, metadata) {
+    if (startTime === 0.0) {
+        startTime = now;
+    }
+    const elapsed = (now - startTime) / 1000.0;
+    const fps = (++paintCount / elapsed).toFixed(3);
+    fpsInfo.innerText = !isFinite(fps) ? 0 : fps;
+    videoCtx.update(elapsed);
+    refVideo.requestVideoFrameCallback(updateVideo)
+}
+
+var lastCalledTime;
+var fps;
+function updateCanvas(time) {
+    if (!lastCalledTime) {
+        lastCalledTime = Date.now();
+        fps = 0;
+    }
+    delta = (Date.now() - lastCalledTime) / 1000;
+    lastCalledTime = Date.now();
+    fps = 1 / delta;
+
+    videoCtx.update(delta);
+    fpsInfo.innerText = !isFinite(fps) ? 0 : fps;
+
+    requestAnimationFrame(updateCanvas);
+}
+
